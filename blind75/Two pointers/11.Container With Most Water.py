d2c3a1f3ee5784d264b2371cc8a5e2c102e68324
mithun-27 <kvl202014@gmail.com>
2026-02-18 22:06:07 +0530

    Neetcode with answers

--- blind75/Two pointers/11.Container With Most Water.py
+++ blind75/Two pointers/11.Container With Most Water.py
@@ -43,3 +43,16 @@ class Solution:
             else:
                 r -= 1
         return res
+    
+#example usage
+solution = Solution()
+print(solution.maxArea([1,8,6,2,5,4,8,3,7]))  # Output: 49
+print(solution.maxArea([1,1]))  # Output: 1
+
+"""walkthrough the code:
+1. We initialize two pointers, `l` and `r`, to the start and end of the `heights` array, respectively. We also initialize a variable `res` to store the maximum area found. 
+2. We enter a while loop that continues until the two pointers meet. Inside the loop, we calculate the area formed by the lines at the `l` and `r` pointers, which is the minimum of the two heights multiplied by the distance between them (r - l). We update `res` if this area is greater than the current maximum.
+3. We then move the pointer that points to the shorter line inward, as this is the only way to potentially find a larger area. If the height at `l` is less than or equal to the height at `r`, we move `l` to the right; otherwise, we move `r` to the left.
+4. Once the pointers meet, we exit the loop and return the maximum area found in `res`.
+This approach efficiently finds the maximum area in O(n) time, where n is the length of the `heights` array, since we are only traversing the array once with the two pointers.
+"""
