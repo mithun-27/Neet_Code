368cb5179aab1c41b4c55da22411952c7b3e1dfe
MITHUN <kvl202014@gmail.com>
2026-02-25 21:55:03 +0530

    neet code with answers

--- blind75/stack/20. Valid Parentheses.py
+++ blind75/stack/20. Valid Parentheses.py
@@ -61,3 +61,9 @@ class Solution:
                 stack.append(c)
 
         return True if not stack else False
+    
+#example usage
+solution = Solution()
+print(solution.isValid("()"))      # True
+print(solution.isValid("()[]{}"))  # True
+print(solution.isValid("(]"))      # False
