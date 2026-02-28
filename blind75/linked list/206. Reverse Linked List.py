8dd8f9fdd2ae2e639b26606371858a77db028bc0
MITHUN <kvl202014@gmail.com>
2026-02-28 21:00:31 +0530

    neet code with answers

--- blind75/linked list/206. Reverse Linked List.py
+++ blind75/linked list/206. Reverse Linked List.py
@@ -23,3 +23,28 @@ Constraints:
 
 The number of nodes in the list is the range [0, 5000].
 -5000 <= Node.val <= 5000"""
+#answer
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, curr = None, head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
+    
+"""walkthrough:
+1. We initialize two pointers, prev and curr, to keep track of the previous and current nodes in the linked list. Initially, prev is set to None and curr is set to the head of the list.
+2. We enter a while loop that continues until curr is None, which means we have reached the end of the list.
+3. Inside the loop, we store the next node of curr in a temporary variable temp. This is necessary because we will be changing the next pointer of curr, and we don't want to lose the reference to the rest of the list.
+4. We then reverse the next pointer of curr by setting curr.next to prev. This effectively reverses the link between curr and prev.
+5. We move the prev pointer to curr, and the curr pointer to temp, which is the next node in the original list.
+6. After the loop, prev will be pointing to the new head of the reversed list, so we return prev.
+"""
