d00720401f35aec22c359f14af0879ef6e4e0ddd
MITHUN <kvl202014@gmail.com>
2026-03-04 23:12:29 +0530

    neet code with answers

--- blind75/linked list/19. Remove Nth Node From End of List.py
+++ blind75/linked list/19. Remove Nth Node From End of List.py
@@ -25,3 +25,27 @@ The number of nodes in the list is sz.
 0 <= Node.val <= 100
 1 <= n <= sz
  """
+
+#answer
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+
+        while n > 0:
+            right = right.next
+            n -= 1
+
+        while right:
+            left = left.next
+            right = right.next
+
+        left.next = left.next.next
+        return dummy.next
