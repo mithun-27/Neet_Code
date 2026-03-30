a3b8d37f711b5aad4111a0832cd0a03caf464752
MITHUN <kvl202014@gmail.com>
2026-03-30 22:38:55 +0530

    neetcode with answers

--- blind75/tries/211. Design Add and Search Words Data Structure.py
+++ blind75/tries/211. Design Add and Search Words Data Structure.py
@@ -34,3 +34,21 @@ word in addWord consists of lowercase English letters.
 word in search consist of '.' or lowercase English letters.
 There will be at most 2 dots in word for search queries.
 At most 104 calls will be made to addWord and search."""
+
+#answer
+class WordDictionary:
+
+    def __init__(self):
+        
+
+    def addWord(self, word: str) -> None:
+        
+
+    def search(self, word: str) -> bool:
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
