5365c635c139f06b334a3ee9075c610817669e9f
MITHUN <kvl202014@gmail.com>
2026-04-02 22:03:42 +0530

    needcode with answers

--- blind75/graphs/200. Number of Islands.py
+++ blind75/graphs/200. Number of Islands.py
@@ -1,26 +1,33 @@
 # 200. Number of Islands
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+"""Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
 
-        rows, cols = len(grid), len(grid[0])
-        visited = [[False] * cols for _ in range(rows)]
-        islands = 0
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 
-        def dfs(r, c):
-            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == "0" or visited[r][c]:
-                return
-            visited[r][c] = True
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
+ 
 
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == "1" and not visited[i][j]:
-                    dfs(i, j)
-                    islands += 1
+Example 1:
 
-        return islands
+Input: grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+Output: 1
+Example 2:
+
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 300
+grid[i][j] is '0' or '1'."""
