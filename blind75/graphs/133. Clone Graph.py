1635e390053aa5f6a1f0ffee7de88f5cb2c73977
MITHUN <kvl202014@gmail.com>
2026-04-03 19:17:42 +0530

    needcode with answers

--- blind75/graphs/133. Clone Graph.py
+++ blind75/graphs/133. Clone Graph.py
@@ -80,3 +80,14 @@ class Solution:
                 oldToNew[cur].neighbors.append(oldToNew[nei])
 
         return oldToNew[node]
+    
+#example usage
+"""
+Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+Output: [[2,4],[1,3],[2,4],[1,3]]
+Explanation: There are 4 nodes in the graph.
+1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+"""
