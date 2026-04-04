02800a3958e952234214752681726aefbcf497b2
MITHUN <kvl202014@gmail.com>
2026-04-04 21:25:22 +0530

    neetcode with answers

--- blind75/graphs/417. Pacific Atlantic Water Flow.py
+++ blind75/graphs/417. Pacific Atlantic Water Flow.py
@@ -43,3 +43,30 @@ m == heights.length
 n == heights[r].length
 1 <= m, n <= 200
 0 <= heights[r][c] <= 105"""
+
+#answer
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        if not heights:
+            return []
+
+        m, n = len(heights), len(heights[0])
+        pacific = set()
+        atlantic = set()
+
+        def dfs(r, c, visited):
+            visited.add((r, c))
+            for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                nr, nc = r + dr, c + dc
+                if (nr, nc) not in visited and 0 <= nr < m and 0 <= nc < n and heights[nr][nc] >= heights[r][c]:
+                    dfs(nr, nc, visited)
+
+        for i in range(m):
+            dfs(i, 0, pacific)
+            dfs(i, n - 1, atlantic)
+
+        for j in range(n):
+            dfs(0, j, pacific)
+            dfs(m - 1, j, atlantic)
+
+        return list(pacific & atlantic)
