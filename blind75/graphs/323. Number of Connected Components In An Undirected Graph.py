b1053c4f4f8e4d1b8af8d3a1a43e5c6af6572b4b
MITHUN <kvl202014@gmail.com>
2026-04-07 22:37:28 +0530

    neetcode with answers

--- blind75/graphs/323. Number of Connected Components In An Undirected Graph.py
+++ blind75/graphs/323. Number of Connected Components In An Undirected Graph.py
@@ -1,1 +1,29 @@
 #323. Number of Connected Components In An Undirected Graph
+"""You have a graph of n nodes. You are given an integer n and an array edges where edges[i] = [aᵢ, bᵢ] indicates that there is an edge between aᵢ and bᵢ in the graph.
+
+Return the number of connected components in the graph.
+
+Example 1:
+
+
+
+Input:
+n = 5, edges = [[0,1],[1,2],[3,4]]
+
+Output: 2
+Example 2:
+
+
+
+Input:
+n = 5, edges = [[0,1],[1,2],[2,3],[3,4]]
+
+Output: 1
+Constraints:
+
+1 <= n <= 2000
+1 <= edges.length <= 5000
+edges[i].length == 2
+0 <= aᵢ <= bᵢ < n
+aᵢ != bᵢ
+There are no repeated edges."""
