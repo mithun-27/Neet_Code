b3bb7e202720e55ec0b5bdaedbd0461ce3b98af9
MITHUN <kvl202014@gmail.com>
2026-04-11 20:55:50 +0530

    Neetcode with answers

--- blind75/1D_dynamic_programming/213. House Robber II.py
+++ blind75/1D_dynamic_programming/213. House Robber II.py
@@ -26,3 +26,19 @@ Constraints:
 
 1 <= nums.length <= 100
 0 <= nums[i] <= 1000"""
+
+#answer
+class Solution:
+
+    def rob(self, nums: List[int]) -> int:
+        return max(nums[0], self.helper(nums[1:]),
+                            self.helper(nums[:-1]))
+
+    def helper(self, nums):
+        rob1, rob2 = 0, 0
+
+        for num in nums:
+            newRob = max(rob1 + num, rob2)
+            rob1 = rob2
+            rob2 = newRob
+        return rob2
