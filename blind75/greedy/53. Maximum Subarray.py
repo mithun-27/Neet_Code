d50a40aec79d52cef3d8a580b77795cad1495fac
MITHUN <kvl202014@gmail.com>
2026-04-24 18:48:17 +0530

    Neetcode with answers

--- blind75/greedy/53. Maximum Subarray.py
+++ blind75/greedy/53. Maximum Subarray.py
@@ -24,3 +24,27 @@ Constraints:
 
 1 <= nums.length <= 105
 -104 <= nums[i] <= 104"""
+
+#answer
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        def dfs(l, r):
+            if l > r:
+                return float("-inf")
+
+            m = (l + r) >> 1
+            leftSum = rightSum = curSum = 0
+            for i in range(m - 1, l - 1, -1):
+                curSum += nums[i]
+                leftSum = max(leftSum, curSum)
+
+            curSum = 0
+            for i in range(m + 1, r + 1):
+                curSum += nums[i]
+                rightSum = max(rightSum, curSum)
+
+            return (max(dfs(l, m - 1),
+                        dfs(m + 1, r),
+                        leftSum + nums[m] + rightSum))
+
+        return dfs(0, len(nums) - 1)
