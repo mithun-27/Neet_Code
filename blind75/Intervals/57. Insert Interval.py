4e04d7ec5fee1ce0ed20397b2c1673444ffae71a
MITHUN <kvl202014@gmail.com>
2026-04-26 22:16:51 +0530

    Neetcode with answers

--- blind75/Intervals/57. Insert Interval.py
+++ blind75/Intervals/57. Insert Interval.py
@@ -28,3 +28,22 @@ intervals[i].length == 2
 intervals is sorted by starti in ascending order.
 newInterval.length == 2
 0 <= start <= end <= 105"""
+
+#answer
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [
+                    min(newInterval[0], intervals[i][0]),
+                    max(newInterval[1], intervals[i][1]),
+                ]
+        res.append(newInterval)
+        return res
