a9509667993bb753bc69b1d034db96b731ecb743
MITHUN <kvl202014@gmail.com>
2026-04-30 23:05:56 +0530

    Neetcode with answers

--- blind75/Intervals/253. Meeting Rooms II.py
+++ blind75/Intervals/253. Meeting Rooms II.py
@@ -22,3 +22,27 @@ Constraints:
 0 <= intervals.length <= 500
 0 <= intervals[i].start < intervals[i].end <= 1,000,000
 """
+
+#answer
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        time = []
+        for i in intervals:
+            time.append((i.start, 1))
+            time.append((i.end, -1))
+
+        time.sort(key=lambda x: (x[0], x[1]))
+
+        res = count = 0
+        for t in time:
+            count += t[1]
+            res = max(res, count)
+        return res
