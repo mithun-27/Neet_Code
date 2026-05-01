1df97e198a4e3730ba100d921b5f2bac1460b12d
MITHUN <kvl202014@gmail.com>
2026-05-01 21:30:27 +0530

    Neetcode with answers

--- blind75/Math&Geomentry/48. Rotate Image.py
+++ blind75/Math&Geomentry/48. Rotate Image.py
@@ -30,3 +30,14 @@ class Solution:
         for i in range(len(matrix)):
             for j in range(i + 1, len(matrix)):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+#example 1: 
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+s = Solution()
+s.rotate(matrix)
+print(matrix)  # Output: [[7,4,1],[8,5,2],[9,6,3]]
+#example 2:
+matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+s = Solution()  
+s.rotate(matrix)
+print(matrix)  # Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
