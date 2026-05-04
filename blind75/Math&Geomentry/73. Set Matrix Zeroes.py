70988a51e53f2986caf6d52c37a1f46c9b5c7c2c
MITHUN <kvl202014@gmail.com>
2026-05-04 21:22:48 +0530

    Neetcode with answers

--- blind75/Math&Geomentry/73. Set Matrix Zeroes.py
+++ blind75/Math&Geomentry/73. Set Matrix Zeroes.py
@@ -57,3 +57,9 @@ matrix = [[1,1,1],[1,0,1],[1,1,1]]
 s = Solution()
 s.setZeroes(matrix)
 print(matrix) # Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+#example 2:
+matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+s = Solution()
+s.setZeroes(matrix)
+print(matrix) # Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]] 
