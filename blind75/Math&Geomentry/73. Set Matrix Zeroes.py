eb7dada4595a2da49fe771e9e747e50f43cf870b
MITHUN <kvl202014@gmail.com>
2026-05-04 21:21:29 +0530

    Neetcode with answers

--- blind75/Math&Geomentry/73. Set Matrix Zeroes.py
+++ blind75/Math&Geomentry/73. Set Matrix Zeroes.py
@@ -23,4 +23,31 @@ m == matrix.length
 n == matrix[0].length
 1 <= m, n <= 200
 -231 <= matrix[i][j] <= 231 - 1
- """
+"""
+#answer
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rowZero = False
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
+
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+
+        if matrix[0][0] == 0:
+            for r in range(ROWS):
+                matrix[r][0] = 0
+
+        if rowZero:
+            for c in range(COLS):
+                matrix[0][c] = 0
