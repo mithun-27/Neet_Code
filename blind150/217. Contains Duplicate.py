75d64e2a7616383f545df8b16ec6cc574ca3248b
MITHUN <kvl202014@gmail.com>
2026-05-11 21:31:33 +0530

    Neetcode with answers

--- blind150/217. Contains Duplicate.py
+++ blind150/217. Contains Duplicate.py
@@ -35,3 +35,13 @@ Constraints:
 
 1 <= nums.length <= 105
 -109 <= nums[i] <= 109"""
+
+#answer
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen=set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
