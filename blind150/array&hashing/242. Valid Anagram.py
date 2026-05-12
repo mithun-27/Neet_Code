1b2b9ffa47a0004a064e2b7d85cbe5632015786f
MITHUN <kvl202014@gmail.com>
2026-05-12 21:02:37 +0530

    Neetcode with answers

--- blind150/array&hashing/242. Valid Anagram.py
+++ blind150/array&hashing/242. Valid Anagram.py
@@ -46,3 +46,8 @@ s = "rat"
 t = "car"
 solution = Solution()
 print(solution.isAnagram(s, t)) # Output: false 
+
+"""walkthrough:
+1. We first check if the lengths of the two strings are different. If they are, we can immediately return false since anagrams must have the same length.                   
+2. We then create two dictionaries, countS and countT, to count the frequency of each character in strings s and t, respectively. We iterate through each character in both strings simultaneously and update the counts in the dictionaries.
+3. Finally, we compare the two dictionaries. If they are equal, it means that both strings have the same characters with the same frequencies, and we return true. Otherwise, we return false."""   
