37c9647080f8144c4432b482c1d1a95538309e00
MITHUN <kvl202014@gmail.com>
2026-05-16 22:04:17 +0530

    Neetcode with answers

--- blind150/array&hashing/347. Top K Frequent Elements.py
+++ blind150/array&hashing/347. Top K Frequent Elements.py
@@ -29,3 +29,21 @@ Constraints:
 -104 <= nums[i] <= 104
 k is in the range [1, the number of unique elements in the array].
 It is guaranteed that the answer is unique."""
+
+#answer
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
