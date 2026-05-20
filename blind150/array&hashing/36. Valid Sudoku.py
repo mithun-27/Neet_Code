6d30d22d246e37ac539db48cc018a3be68de614c
MITHUN <kvl202014@gmail.com>
2026-05-20 21:23:14 +0530

    Neetcode with answers

--- blind150/array&hashing/36. Valid Sudoku.py
+++ blind150/array&hashing/36. Valid Sudoku.py
@@ -45,3 +45,29 @@ Constraints:
 board.length == 9
 board[i].length == 9
 board[i][j] is a digit 1-9 or '.'."""
+
+#answer
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [0] * 9
+        cols = [0] * 9
+        squares = [0] * 9
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+
+                val = int(board[r][c]) - 1
+                if (1 << val) & rows[r]:
+                    return False
+                if (1 << val) & cols[c]:
+                    return False
+                if (1 << val) & squares[(r // 3) * 3 + (c // 3)]:
+                    return False
+
+                rows[r] |= (1 << val)
+                cols[c] |= (1 << val)
+                squares[(r // 3) * 3 + (c // 3)] |= (1 << val)
+
+        return True
