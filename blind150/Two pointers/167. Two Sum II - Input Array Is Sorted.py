53d37732bb5f4ded531522067a1a1eac6333bb6b
MITHUN <kvl202014@gmail.com>
2026-05-27 21:18:49 +0530

    Neetcode with answers

--- blind150/Two pointers/167. Two Sum II - Input Array Is Sorted.py
+++ blind150/Two pointers/167. Two Sum II - Input Array Is Sorted.py
@@ -33,3 +33,19 @@ Constraints:
 numbers is sorted in non-decreasing order.
 -1000 <= target <= 1000
 The tests are generated such that there is exactly one solution."""
+
+#answer
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
+        return []
