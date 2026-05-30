b6b3267336d032a9df0b76ae0363f0aae490b441
MITHUN <kvl202014@gmail.com>
2026-05-30 19:27:24 +0530

    Neetcode with answers

--- blind150/Two pointers/42. Trapping Rain Water.py
+++ blind150/Two pointers/42. Trapping Rain Water.py
@@ -20,3 +20,23 @@ Constraints:
 n == height.length
 1 <= n <= 2 * 104
 0 <= height[i] <= 105"""
+
+#answer
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+        return res
