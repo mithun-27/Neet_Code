8151694c0cd78b8f8a87551ae16ae113a5a612b5
MITHUN <kvl202014@gmail.com>
2026-06-01 21:20:15 +0530

    Neetcode with answers

--- blind150/Sliding Windows/3. Longest Substring Without Repeating Characters.py
+++ blind150/Sliding Windows/3. Longest Substring Without Repeating Characters.py
@@ -25,3 +25,18 @@ Constraints:
 
 0 <= s.length <= 5 * 104
 s consists of English letters, digits, symbols and spaces."""
+
+#answer
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res
