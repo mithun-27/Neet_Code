b7978038c72c34158a6941f5cc23478f924bff02
MITHUN <kvl202014@gmail.com>
2026-06-04 22:06:58 +0530

    Neetcode with answers

--- blind150/Sliding Windows/239. Sliding Window Maximum.py
+++ blind150/Sliding Windows/239. Sliding Window Maximum.py
@@ -29,3 +29,25 @@ Constraints:
 1 <= nums.length <= 105
 -104 <= nums[i] <= 104
 1 <= k <= nums.length"""
+
+#answer
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+        q = deque()  # index
+        l = r = 0
+
+        while r < len(nums):
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            if l > q[0]:
+                q.popleft()
+
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
+
+        return output
