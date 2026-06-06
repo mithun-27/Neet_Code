accceff34b8086e72a744c5911d5bd103e0bddab
MITHUN <kvl202014@gmail.com>
2026-06-06 22:06:24 +0530

    Neetcode with answers

--- blind150/stack/155. Min Stack.py
+++ blind150/stack/155. Min Stack.py
@@ -37,3 +37,37 @@ Constraints:
 -231 <= val <= 231 - 1
 Methods pop, top and getMin operations will always be called on non-empty stacks.
 At most 3 * 104 calls will be made to push, pop, top, and getMin."""
+
+#answer
+class MinStack:
+    def __init__(self):
+        self.min = float('inf')
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append(0)
+            self.min = val
+        else:
+            self.stack.append(val - self.min)
+            if val < self.min:
+                self.min = val
+
+    def pop(self) -> None:
+        if not self.stack:
+            return
+
+        pop = self.stack.pop()
+
+        if pop < 0:
+            self.min = self.min - pop
+
+    def top(self) -> int:
+        top = self.stack[-1]
+        if top > 0:
+            return top + self.min
+        else:
+            return self.min
+
+    def getMin(self) -> int:
+        return self.min
