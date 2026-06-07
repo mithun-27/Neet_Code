fd47bc4fad382cf8d7a9f7238a023d058584659b
MITHUN <kvl202014@gmail.com>
2026-06-07 21:03:44 +0530

    Neetcode with answers

--- blind150/stack/150. Evaluate Reverse Polish Notation.py
+++ blind150/stack/150. Evaluate Reverse Polish Notation.py
@@ -59,3 +59,22 @@ class Solution:
             else:
                 stack.append(int(c))
         return stack[0]
+    
+#example 1:
+#Input: tokens = ["2","1","+","3","*"]
+#Output: 9
+#Explanation: ((2 + 1) * 3) = 9
+#example 2:
+#Input: tokens = ["4","13","5","/","+"] 
+#Output: 6
+#Explanation: (4 + (13 / 5)) = 6    
+#example 3:
+#Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+#Output: 22
+#Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+#= ((10 * (6 / (12 * -11))) + 17) + 5
+#= ((10 * (6 / -132)) + 17) + 5 
+#= ((10 * 0) + 17) + 5
+#= (0 + 17) + 5 
+#= 17 + 5
+#= 22
