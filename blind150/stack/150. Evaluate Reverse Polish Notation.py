84d4be820beb77a44fdc92fd78f8f3851cf1a25e
MITHUN <kvl202014@gmail.com>
2026-06-07 21:02:48 +0530

    Neetcode with answers

--- blind150/stack/150. Evaluate Reverse Polish Notation.py
+++ blind150/stack/150. Evaluate Reverse Polish Notation.py
@@ -40,3 +40,22 @@ Constraints:
 
 1 <= tokens.length <= 104
 tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200]."""
+
+#answer
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(float(b) / a))
+            else:
+                stack.append(int(c))
+        return stack[0]
