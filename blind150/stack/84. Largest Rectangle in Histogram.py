4be37b1fbd2ed29267ad56c4166a89099a41e56f
MITHUN <kvl202014@gmail.com>
2026-06-10 22:49:12 +0530

    Neetcode with answers

--- blind150/stack/84. Largest Rectangle in Histogram.py
+++ blind150/stack/84. Largest Rectangle in Histogram.py
@@ -21,3 +21,19 @@ Constraints:
 
 1 <= heights.length <= 105
 0 <= heights[i] <= 104"""
+
+#answer
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        n = len(heights)
+        maxArea = 0
+        stack = []
+
+        for i in range(n + 1):
+            while stack and (i == n  or heights[stack[-1]] >= heights[i]):
+                height = heights[stack.pop()]
+                width = i if not stack else i - stack[-1] - 1
+                maxArea = max(maxArea, height * width)
+            stack.append(i)
+        return maxArea
+    
