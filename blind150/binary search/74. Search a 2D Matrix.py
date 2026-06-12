b16e1127560faddf2df008be6e86b2766b10bccf
MITHUN <kvl202014@gmail.com>
2026-06-12 22:29:08 +0530

    Neetcode with answers

--- blind150/binary search/74. Search a 2D Matrix.py
+++ blind150/binary search/74. Search a 2D Matrix.py
@@ -44,3 +44,11 @@ class Solution:
             else:
                 return True
         return False
+    
+
+#example 1:
+"""Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true  """
+#example 2:
+"""Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false  """  
