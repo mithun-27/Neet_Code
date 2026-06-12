974a2c38a4b182cfccfa3b01981407e79dd71b89
MITHUN <kvl202014@gmail.com>
2026-06-12 22:28:39 +0530

    Neetcode with answers

--- blind150/binary search/74. Search a 2D Matrix.py
+++ blind150/binary search/74. Search a 2D Matrix.py
@@ -27,3 +27,20 @@ m == matrix.length
 n == matrix[i].length
 1 <= m, n <= 100
 -104 <= matrix[i][j], target <= 104"""
+
+#answer
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        l, r = 0, ROWS * COLS - 1
+        while l <= r:
+            m = l + (r - l) // 2
+            row, col = m // COLS, m % COLS
+            if target > matrix[row][col]:
+                l = m + 1
+            elif target < matrix[row][col]:
+                r = m - 1
+            else:
+                return True
+        return False
