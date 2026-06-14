4b25f94f8bebc8a87b9fc16d78dddf4f95adb0a4
MITHUN <kvl202014@gmail.com>
2026-06-14 22:58:05 +0530

    Neetcode with answers

--- blind150/binary search/153. Find Minimum in Rotated Sorted Array.py
+++ blind150/binary search/153. Find Minimum in Rotated Sorted Array.py
@@ -47,3 +47,16 @@ class Solution:
             else:
                 l = m + 1
         return nums[l]
+    
+#example 1:
+"""Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.  """
+#example 2: 
+"""Input: nums = [4,5,6,7,0,1,2]
+Output: 0
+Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.  """
+#example 3:
+"""Input: nums = [11,13,15,17]
+Output: 11
+Explanation: The original array was [11,13,15,17] and it was rotated 4 times.  """  
