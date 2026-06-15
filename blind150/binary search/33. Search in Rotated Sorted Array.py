9358d35e8d178ab8adb8a8d4349a567842d92559
MITHUN <kvl202014@gmail.com>
2026-06-15 22:14:14 +0530

    Neetcode with answers

--- blind150/binary search/33. Search in Rotated Sorted Array.py
+++ blind150/binary search/33. Search in Rotated Sorted Array.py
@@ -30,3 +30,33 @@ Constraints:
 All values of nums are unique.
 nums is an ascending array that is possibly rotated.
 -104 <= target <= 104"""
+
+#answer
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
+    
+#example 1:
+"""Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4"""
+#example 2:
+"""Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1"""   
