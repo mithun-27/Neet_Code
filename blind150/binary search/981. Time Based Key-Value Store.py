933214fd30374fbc07f8ed787e52bd4c3beb8bf2
MITHUN <kvl202014@gmail.com>
2026-06-16 22:16:13 +0530

    Neetcode with answers

--- blind150/binary search/981. Time Based Key-Value Store.py
+++ blind150/binary search/981. Time Based Key-Value Store.py
@@ -33,3 +33,26 @@ key and value consist of lowercase English letters and digits.
 1 <= timestamp <= 107
 All the timestamps timestamp of set are strictly increasing.
 At most 2 * 105 calls will be made to set and get."""
+
+#answer
+class TimeMap:
+
+    def __init__(self):
+        self.keyStore = {}  # key : list of [val, timestamp]
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.keyStore:
+            self.keyStore[key] = []
+        self.keyStore[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res, values = "", self.keyStore.get(key, [])
+        l, r = 0, len(values) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+        return res
