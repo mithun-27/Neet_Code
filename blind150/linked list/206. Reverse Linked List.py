be80ffa62ef40f74fbbee337b2cd05f46554e030
MITHUN <kvl202014@gmail.com>
2026-06-18 21:37:19 +0530

    Neetcode with answers

--- blind150/linked list/206. Reverse Linked List.py
+++ blind150/linked list/206. Reverse Linked List.py
@@ -23,3 +23,20 @@ Constraints:
 
 The number of nodes in the list is the range [0, 5000].
 -5000 <= Node.val <= 5000"""
+
+#answer
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev, curr = None, head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
