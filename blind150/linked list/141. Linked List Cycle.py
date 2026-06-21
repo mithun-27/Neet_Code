d0bcfa805bc0546e2ba3598d26b65fed0ec23cba
MITHUN <kvl202014@gmail.com>
2026-06-21 20:49:24 +0530

    Neetcode with answers

--- blind150/linked list/141. Linked List Cycle.py
+++ blind150/linked list/141. Linked List Cycle.py
@@ -51,3 +51,16 @@ class Solution:
             if slow == fast:
                 return True
         return False
+    
+#example 1:
+"""Input: head = [3,2,0,-4], pos = 1    
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed)."""
+#example 2:
+"""Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node."""
+#example 3:
+"""Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list."""
