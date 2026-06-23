c008549f4d13b2dde6be16ed4b4dce73f616d906
MITHUN <kvl202014@gmail.com>
2026-06-23 20:42:32 +0530

    Neetcode with answers

--- blind150/linked list/19. Remove Nth Node From End of List.py
+++ blind150/linked list/19. Remove Nth Node From End of List.py
@@ -61,3 +61,13 @@ Output: []"""
 #example 3:
 """Input: head = [1,2], n = 1   
 Output: [1]"""  
+
+
+"""walkthrough:
+1. Create a dummy node that points to the head of the list. This helps handle edge cases, such as when the head needs to be removed.
+2. Initialize two pointers, left and right. Set left to the dummy node and right to the head of the list.
+3. Move the right pointer n steps ahead in the list. This creates a gap of n nodes between the left and right pointers.
+4. Move both pointers (left and right) one step at a time until the right pointer reaches the end of the list. At this point, the left pointer will be just before the node that needs to be removed.
+5. Remove the nth node from the end by changing the next pointer of the left node to skip the node to be removed (i.e., left.next = left.next.next).
+6. Return the next node of the dummy node, which is the new head of the modified list. This handles cases where the head of the list was removed.       
+"""
