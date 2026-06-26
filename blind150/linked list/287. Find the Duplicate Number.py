a7f2cb5a38c6ddf5eb57f27ddb2503370acc1f3b
MITHUN <kvl202014@gmail.com>
2026-06-26 22:39:52 +0530

    Neetcode with answers

--- blind150/linked list/287. Find the Duplicate Number.py
+++ blind150/linked list/287. Find the Duplicate Number.py
@@ -35,3 +35,18 @@ How can we prove that at least one duplicate number must exist in nums?
 Can you solve the problem in linear runtime complexity?"""
 
 #answer
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
