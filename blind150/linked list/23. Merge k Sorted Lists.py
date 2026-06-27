e0a047f586d08d9891733b67b0b7c17949ba19d4
MITHUN <kvl202014@gmail.com>
2026-06-28 00:57:12 +0530

    Neetcode with answers

--- blind150/linked list/23. Merge k Sorted Lists.py
+++ blind150/linked list/23. Merge k Sorted Lists.py
@@ -77,3 +77,10 @@ class Solution:
             tail.next = l2
 
         return dummy.next
+    
+#example:1
+"""Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]"""
+#example:2
+"""Input: lists = []
+Output: []"""
