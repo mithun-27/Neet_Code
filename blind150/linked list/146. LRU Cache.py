cfaa7e152283ea408466872f747e38c8df48fecf
MITHUN <kvl202014@gmail.com>
2026-06-27 23:09:54 +0530

    Neetcode with answers

--- blind150/linked list/146. LRU Cache.py
+++ blind150/linked list/146. LRU Cache.py
@@ -37,3 +37,24 @@ Constraints:
 0 <= key <= 104
 0 <= value <= 105
 At most 2 * 105 calls will be made to get and put."""
+
+#answer
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.cap = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+
+        if len(self.cache) > self.cap:
+            self.cache.popitem(last=False)
