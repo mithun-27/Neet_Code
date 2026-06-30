48d9bc6fab40c2f4aa72b14d96070977eb59f75a
MITHUN <kvl202014@gmail.com>
2026-06-30 21:41:54 +0530

    Neetcode with answers

--- blind150/Trees/226. Invert Binary Tree.py
+++ blind150/Trees/226. Invert Binary Tree.py
@@ -45,3 +45,14 @@ class Solution:
             if node.right:
                 stack.append(node.right)
         return root
+    
+
+#example 1:
+"""Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1] """
+#example 2:
+"""Input: root = [2,1,3]    
+Output: [2,3,1] """
+#example 3:
+"""Input: root = []
+Output: [] """
