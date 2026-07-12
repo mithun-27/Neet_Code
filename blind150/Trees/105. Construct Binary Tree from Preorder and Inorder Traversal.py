e851a50b6b9828d49950687cc7972e8d82a4321b
MITHUN <kvl202014@gmail.com>
2026-07-12 22:52:24 +0530

    neet code with answers

--- blind150/Trees/105. Construct Binary Tree from Preorder and Inorder Traversal.py
+++ blind150/Trees/105. Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -53,3 +53,13 @@ class Solution:
                 j += 1
         
         return head.right
+    
+#example 1:
+"""Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]"""
+#example 2:
+"""Input: preorder = [-1], inorder = [-1]
+Output: [-1]"""
+#example 3:
+"""Input: preorder = [1,2], inorder = [2,1]
+Output: [1,2]"""
