01cab194e16b2e4a260c3aa0ddf89ec275301997
MITHUN <kvl202014@gmail.com>
2026-07-15 22:04:46 +0530

    neet code with answers

--- blind150/heap and priority queue/703. Kth Largest Element in a Stream.py
+++ blind150/heap and priority queue/703. Kth Largest Element in a Stream.py
@@ -50,3 +50,18 @@ Constraints:
 -104 <= nums[i] <= 104
 -104 <= val <= 104
 At most 104 calls will be made to add."""
+
+#answer
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
