50508c1216e4abb49485225b68429e14b1975e97
MITHUN <kvl202014@gmail.com>
2026-07-19 20:59:37 +0530

    neet code with answers

--- blind150/heap and priority queue/621. Task Scheduler.py
+++ blind150/heap and priority queue/621. Task Scheduler.py
@@ -42,3 +42,18 @@ Constraints:
 1 <= tasks.length <= 104
 tasks[i] is an uppercase English letter.
 0 <= n <= 100"""
+
+#answer
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        count = [0] * 26
+        for task in tasks:
+            count[ord(task) - ord('A')] += 1
+
+        maxf = max(count)
+        maxCount = 0
+        for i in count:
+            maxCount += 1 if i == maxf else 0
+
+        time = (maxf - 1) * (n + 1) + maxCount
+        return max(len(tasks), time)
