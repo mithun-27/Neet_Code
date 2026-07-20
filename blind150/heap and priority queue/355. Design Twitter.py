c513dd73964fd0b2f4293f4f804defe2bc9bb514
MITHUN <kvl202014@gmail.com>
2026-07-20 21:57:17 +0530

    neet code with answers

--- blind150/heap and priority queue/355. Design Twitter.py
+++ blind150/heap and priority queue/355. Design Twitter.py
@@ -89,3 +89,14 @@ class Twitter:
     def unfollow(self, followerId: int, followeeId: int) -> None:
         if followeeId in self.followMap[followerId]:
             self.followMap[followerId].remove(followeeId)
+
+
+#example 1:
+twitter = Twitter()
+twitter.postTweet(1, 5)  # User 1 posts a new tweet (id = 5).
+print(twitter.getNewsFeed(1))  # User 1's news feed should return a list with 1 tweet id -> [5]. return [5]
+twitter.follow(1, 2)  # User 1 follows user 2.  
+twitter.postTweet(2, 6)  # User 2 posts a new tweet (id = 6).   
+print(twitter.getNewsFeed(1))  # User 1's news feed should return a list with 2 tweet ids -> [6, 5]. Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+twitter.unfollow(1, 2)  # User 1 unfollows user 2   
+print(twitter.getNewsFeed(1))  # User 1's news feed should return a list with 1 tweet id -> [5], since user 1 is no longer following user 2.    
