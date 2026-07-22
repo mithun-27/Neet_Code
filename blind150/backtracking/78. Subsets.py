7ec29237955bf6b0d89e39b53ed168f0591e9c8d
MITHUN <kvl202014@gmail.com>
2026-07-22 21:48:25 +0530

    neet code with answers

--- blind150/backtracking/78. Subsets.py
+++ blind150/backtracking/78. Subsets.py
@@ -1,9 +1,24 @@
 #78. Subsets
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        res = []
-        for i in range(1 << n):
-            subset = [nums[j] for j in range(n) if (i & (1 << j))]
-            res.append(subset)
-        return res
+"""Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+All the numbers of nums are unique.
+ 
+"""
