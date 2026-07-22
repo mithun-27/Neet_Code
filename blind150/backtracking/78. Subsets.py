97eadc6feb7baf60c145008ef33394cb929609c4
MITHUN <kvl202014@gmail.com>
2026-07-22 21:48:43 +0530

    neet code with answers

--- blind150/backtracking/78. Subsets.py
+++ blind150/backtracking/78. Subsets.py
@@ -24,3 +24,11 @@ All the numbers of nums are unique.
 """
 
 #answer
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = []
+        for i in range(1 << n):
+            subset = [nums[j] for j in range(n) if (i & (1 << j))]
+            res.append(subset)
+        return res
