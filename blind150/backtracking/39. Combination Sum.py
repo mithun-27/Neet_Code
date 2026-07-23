023eb33a8bd598b4049fce0a6d69c1d045e4c0a6
MITHUN <kvl202014@gmail.com>
2026-07-23 22:10:54 +0530

    neet code with answers

--- blind150/backtracking/39. Combination Sum.py
+++ blind150/backtracking/39. Combination Sum.py
@@ -31,3 +31,24 @@ Constraints:
 2 <= candidates[i] <= 40
 All elements of candidates are distinct.
 1 <= target <= 40"""
+
+#answer
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        def dfs(i, cur, total):
+            if total == target:
+                res.append(cur.copy())
+                return
+
+            for j in range(i, len(nums)):
+                if total + nums[j] > target:
+                    return
+                cur.append(nums[j])
+                dfs(j, cur, total + nums[j])
+                cur.pop()
+
+        dfs(0, [], 0)
+        return res
