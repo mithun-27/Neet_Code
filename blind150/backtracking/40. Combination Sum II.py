13902bcfcc41426fbd80b2a01d6b1e80342abba3
MITHUN <kvl202014@gmail.com>
2026-07-24 21:00:24 +0530

    neet code with answers

--- blind150/backtracking/40. Combination Sum II.py
+++ blind150/backtracking/40. Combination Sum II.py
@@ -32,3 +32,26 @@ Constraints:
 1 <= candidates.length <= 100
 1 <= candidates[i] <= 50
 1 <= target <= 30"""
+
+#answer
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+
+        def dfs(idx, path, cur):
+            if cur == target:
+                res.append(path.copy())
+                return
+            for i in range(idx, len(candidates)):
+                if i > idx and candidates[i] == candidates[i - 1]:
+                    continue
+                if cur + candidates[i] > target:
+                    break
+
+                path.append(candidates[i])
+                dfs(i + 1, path, cur + candidates[i])
+                path.pop()
+
+        dfs(0, [], 0)
+        return res
