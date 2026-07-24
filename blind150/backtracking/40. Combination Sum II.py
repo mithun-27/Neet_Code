3e334912a3ac2f221421a458ff2b05131c18fe32
MITHUN <kvl202014@gmail.com>
2026-07-24 21:03:40 +0530

    neet code with answers

--- blind150/backtracking/40. Combination Sum II.py
+++ blind150/backtracking/40. Combination Sum II.py
@@ -55,3 +55,24 @@ class Solution:
 
         dfs(0, [], 0)
         return res
+
+#example
+"""Example 1:
+
+Input: candidates = [10,1,2,7,6,1,5], target = 8
+Output: 
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+Example 2:
+
+Input: candidates = [2,5,2,1,2], target = 5
+Output: 
+[
+[1,2,2],
+[5]
+]
+ """
