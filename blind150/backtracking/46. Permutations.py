1719ad675e4af192ff3233ad5367593cc1724cc0
MITHUN <kvl202014@gmail.com>
2026-07-25 22:11:18 +0530

    neet code with answers

--- blind150/backtracking/46. Permutations.py
+++ blind150/backtracking/46. Permutations.py
@@ -22,3 +22,19 @@ Constraints:
 1 <= nums.length <= 6
 -10 <= nums[i] <= 10
 All the integers of nums are unique."""
+
+#answer
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        self.backtrack(nums, 0)
+        return self.res
+
+    def backtrack(self, nums: List[int], idx: int):
+        if idx == len(nums):
+            self.res.append(nums[:])
+            return
+        for i in range(idx, len(nums)):
+            nums[idx], nums[i] = nums[i], nums[idx]
+            self.backtrack(nums, idx + 1)
+            nums[idx], nums[i] = nums[i], nums[idx]
