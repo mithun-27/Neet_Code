81b82347f7cdf63eb1589af4d813b87304aca902
MITHUN <kvl202014@gmail.com>
2026-07-27 19:52:35 +0530

    neet code with answers

--- blind150/backtracking/22. Generate Parentheses.py
+++ blind150/backtracking/22. Generate Parentheses.py
@@ -16,3 +16,17 @@ Output: ["()"]
 Constraints:
 
 1 <= n <= 8"""
+
+#answer
+class Solution:
+    def generateParenthesis(self, n):
+        res = [[] for _ in range(n+1)]
+        res[0] = [""]
+
+        for k in range(n + 1):
+            for i in range(k):
+                for left in res[i]:
+                    for right in res[k-i-1]:
+                        res[k].append("(" + left + ")" + right)
+
+        return res[-1]
