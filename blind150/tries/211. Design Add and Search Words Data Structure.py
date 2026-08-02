a4050efa738714a2d8881bc88b5516cb4b28d044
MITHUN <kvl202014@gmail.com>
2026-08-02 21:51:20 +0530

    neet code with answers

--- blind150/tries/211. Design Add and Search Words Data Structure.py
+++ blind150/tries/211. Design Add and Search Words Data Structure.py
@@ -76,3 +76,15 @@ Output
 [null,null,null,null,false,true,true,true]
 Expected
 [null,null,null,null,false,true,true,true]"""
+
+
+"""Walkthrough:
+1. We want to design a data structure that supports adding words and searching for words, where the search pattern may contain the wildcard character `'.'`, which can match any single lowercase letter.
+2. We use a Trie (Prefix Tree) to store all the words. Each Trie node contains a collection of child nodes and a boolean flag indicating whether the current node represents the end of a valid word.
+3. During the `addWord()` operation, we start from the root node and process each character of the word one by one. If a child node for the current character does not exist, we create it and move to that child node.
+4. After inserting the last character, we mark the current node as the end of a word. This allows us to distinguish complete words from prefixes.
+5. During the `search()` operation, we perform a depth-first search (DFS) on the Trie. We examine one character of the search pattern at a time while traversing the Trie.
+6. If the current character is a lowercase letter, we simply move to the corresponding child node. If the child does not exist, we return `False` because no matching word is possible.
+7. If the current character is `'.'`, it can represent any letter. Therefore, we recursively search every child node of the current Trie node. If any recursive call returns `True`, the search is successful.
+8. When all characters in the search pattern have been processed, we return `True` only if the current Trie node is marked as the end of a valid word. Otherwise, we return `False`.
+9. Let `L` be the length of the search word. The time complexity of `addWord()` is `O(L)`. The time complexity of `search()` is `O(L)` when there are no wildcards, but in the worst case it becomes exponential because `'.'` may branch into multiple recursive searches. The auxiliary space complexity is `O(L)` for the recursion stack during the search (excluding the space required to store the Trie)."""
