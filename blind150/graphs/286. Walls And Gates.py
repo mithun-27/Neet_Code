84e3e5da88051321068ee9c1a7c22bb759d13365
MITHUN <kvl202014@gmail.com>
2026-08-07 22:49:21 +0530

    neet code with answers

--- blind150/graphs/286. Walls And Gates.py
+++ blind150/graphs/286. Walls And Gates.py
@@ -46,3 +46,35 @@ m == grid.length
 n == grid[i].length
 1 <= m, n <= 100
 grid[i][j] is one of {-1, 0, 2147483647}"""
+
+#answer:
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+        q = deque()
+
+        def addCell(r, c):
+            if (min(r, c) < 0 or r == ROWS or c == COLS or
+                (r, c) in visit or grid[r][c] == -1
+            ):
+                return
+            visit.add((r, c))
+            q.append([r, c])
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0:
+                    q.append([r, c])
+                    visit.add((r, c))
+
+        dist = 0
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                grid[r][c] = dist
+                addCell(r + 1, c)
+                addCell(r - 1, c)
+                addCell(r, c + 1)
+                addCell(r, c - 1)
+            dist += 1
