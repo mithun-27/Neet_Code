fb22d323c9a58c0fcd0951be3e2d599229e9e645
MITHUN <kvl202014@gmail.com>
2026-08-12 21:43:39 +0530

    neet code with answers

--- blind150/graphs/210. Course Schedule II.py
+++ blind150/graphs/210. Course Schedule II.py
@@ -33,3 +33,26 @@ ai != bi
 All the pairs [ai, bi] are distinct."""
 
 #answer:
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = [[] for i in range(numCourses)]
+        indegree = [0] * numCourses
+        for nxt, pre in prerequisites:
+            indegree[nxt] += 1
+            adj[pre].append(nxt)
+
+        output = []
+
+        def dfs(node):
+            output.append(node)
+            indegree[node] -= 1
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    dfs(nei)
+
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                dfs(i)
+
+        return output if len(output) == numCourses else []
