0287d13a9221f1f5a6f9699894ef60aaa8d00195
MITHUN <kvl202014@gmail.com>
2026-08-24 21:22:50 +0530

    neet code with answers

--- blind150/1D-dynamic programming/746. Min Cost Climbing Stairs.py
+++ blind150/1D-dynamic programming/746. Min Cost Climbing Stairs.py
@@ -32,3 +32,11 @@ Constraints:
 
 2 <= cost.length <= 1000
 0 <= cost[i] <= 999"""
+
+#answer :
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
+
+        return min(cost[0], cost[1])
