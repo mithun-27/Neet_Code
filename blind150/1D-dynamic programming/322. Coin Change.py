8b19d1e949337fcc5a44ec8626752850f34aca79
MITHUN <kvl202014@gmail.com>
2026-08-30 22:32:41 +0530

    neet code with answers

--- blind150/1D-dynamic programming/322. Coin Change.py
+++ blind150/1D-dynamic programming/322. Coin Change.py
@@ -27,3 +27,29 @@ Constraints:
 1 <= coins.length <= 12
 1 <= coins[i] <= 231 - 1
 0 <= amount <= 104"""
+
+#answer:
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+
+        q = deque([0])
+        seen = [False] * (amount + 1)
+        seen[0] = True
+        res = 0
+
+        while q:
+            res += 1
+            for _ in range(len(q)):
+                cur = q.popleft()
+                for coin in coins:
+                    nxt = cur + coin
+                    if nxt == amount:
+                        return res
+                    if nxt > amount or seen[nxt]:
+                        continue
+                    seen[nxt] = True
+                    q.append(nxt)
+
+        return -1   
