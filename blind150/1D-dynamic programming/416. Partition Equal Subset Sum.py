dc21752b5bb1bc7b6324146dd19fd9ff30e58a08
MITHUN <kvl202014@gmail.com>
2026-09-03 19:58:02 +0530

    neet code with answers

--- blind150/1D-dynamic programming/416. Partition Equal Subset Sum.py
+++ blind150/1D-dynamic programming/416. Partition Equal Subset Sum.py
@@ -19,3 +19,18 @@ Constraints:
 
 1 <= nums.length <= 200
 1 <= nums[i] <= 100"""
+
+#answer:
+class Solution:
+    def canPartition(self, nums: list[int]) -> bool:
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+
+        target = total // 2
+        dp = 1 << 0
+
+        for num in nums:
+            dp |= dp << num
+
+        return (dp & (1 << target)) != 0
