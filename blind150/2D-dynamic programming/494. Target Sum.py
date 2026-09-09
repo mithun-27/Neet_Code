cbf8fac938f98b1a2d6120488f42907b4f01cf7e
MITHUN <kvl202014@gmail.com>
2026-09-09 22:46:54 +0530

    neet code with answers

--- blind150/2D-dynamic programming/494. Target Sum.py
+++ blind150/2D-dynamic programming/494. Target Sum.py
@@ -30,3 +30,18 @@ Constraints:
 0 <= nums[i] <= 1000
 0 <= sum(nums[i]) <= 1000
 -1000 <= target <= 1000"""
+
+#answer:
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp = defaultdict(int)
+        dp[0] = 1
+
+        for num in nums:
+            next_dp = defaultdict(int)
+            for total, count in dp.items():
+                next_dp[total + num] += count
+                next_dp[total - num] += count
+            dp = next_dp
+
+        return dp[target]
