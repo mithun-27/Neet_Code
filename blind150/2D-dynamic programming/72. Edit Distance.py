29ac7ed212954288b2a27bc6925c24842293385e
MITHUN <kvl202014@gmail.com>
2026-09-13 19:33:25 +0530

    neet code with answers

--- blind150/2D-dynamic programming/72. Edit Distance.py
+++ blind150/2D-dynamic programming/72. Edit Distance.py
@@ -32,3 +32,26 @@ Constraints:
 
 0 <= word1.length, word2.length <= 500
 word1 and word2 consist of lowercase English letters."""
+
+
+#answer:
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        if m < n:
+            m, n = n, m
+            word1, word2 = word2, word1
+
+        dp = [n - i for i in range(n + 1)]
+
+        for i in range(m - 1, -1, -1):
+            nextDp = dp[n]
+            dp[n] = m - i
+            for j in range(n - 1, -1, -1):
+                temp = dp[j]
+                if word1[i] == word2[j]:
+                    dp[j] = nextDp
+                else:
+                    dp[j] = 1 + min(dp[j], dp[j + 1], nextDp)
+                nextDp = temp
+        return dp[0]
