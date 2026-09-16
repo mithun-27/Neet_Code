afb8b9a84d3e68b6e3cd1e916ff424894e6da560
MITHUN <kvl202014@gmail.com>
2026-09-16 22:54:12 +0530

    neet code with answers

--- blind150/2D-dynamic programming/10. Regular Expression Matching.py
+++ blind150/2D-dynamic programming/10. Regular Expression Matching.py
@@ -31,3 +31,27 @@ Constraints:
 s contains only lowercase English letters.
 p contains only lowercase English letters, '.', and '*'.
 It is guaranteed for each appearance of the character '*', there will be a previous valid character to match."""
+
+
+#answer:
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [False] * (len(p) + 1)
+        dp[len(p)] = True
+
+        for i in range(len(s), -1, -1):
+            dp1 = dp[len(p)]
+            dp[len(p)] = (i == len(s))
+
+            for j in range(len(p) - 1, -1, -1):
+                match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+                res = False
+                if (j + 1) < len(p) and p[j + 1] == "*":
+                    res = dp[j + 2]
+                    if match:
+                        res |= dp[j]
+                elif match:
+                    res = dp1
+                dp[j], dp1 = res, dp[j]
+
+        return dp[0]
