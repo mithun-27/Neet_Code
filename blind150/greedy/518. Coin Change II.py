add61bf55039bcbe6d3a9180c3a3bf567bb46cf9
MITHUN <kvl202014@gmail.com>
2026-09-20 18:15:46 +0530

    neet code with answers

--- blind150/greedy/518. Coin Change II.py
+++ blind150/greedy/518. Coin Change II.py
@@ -35,3 +35,14 @@ Constraints:
 1 <= coins[i] <= 5000
 All the values of coins are unique.
 0 <= amount <= 5000"""
+
+
+#answer:
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for i in range(len(coins) - 1, -1, -1):
+            for a in range(1, amount + 1):
+                dp[a] += dp[a - coins[i]] if coins[i] <= a else 0
+        return dp[amount]
