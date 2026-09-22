d0858667e27309312a1e54192aff83c16da3ce46
MITHUN <kvl202014@gmail.com>
2026-09-22 21:32:27 +0530

    neet code with answers

--- blind150/greedy/134. Gas Station.py
+++ blind150/greedy/134. Gas Station.py
@@ -38,3 +38,21 @@ n == gas.length == cost.length
 1 <= n <= 105
 0 <= gas[i], cost[i] <= 104
 The input is generated such that the answer is unique."""
+
+
+#answer:
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+
+        total = 0
+        res = 0
+        for i in range(len(gas)):
+            total += (gas[i] - cost[i])
+
+            if total < 0:
+                total = 0
+                res = i + 1
+
+        return res
