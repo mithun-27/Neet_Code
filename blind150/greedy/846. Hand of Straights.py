e7e382417a6d1f8e1ee1b4a9362e296c13c6e41a
MITHUN <kvl202014@gmail.com>
2026-09-23 21:06:15 +0530

    neet code with answers

--- blind150/greedy/846. Hand of Straights.py
+++ blind150/greedy/846. Hand of Straights.py
@@ -23,3 +23,24 @@ Constraints:
 1 <= hand.length <= 104
 0 <= hand[i] <= 109
 1 <= groupSize <= hand.length"""
+
+
+#answer:
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+
+        count = Counter(hand)
+        for num in hand:
+            start = num
+            while count[start - 1]:
+                start -= 1
+            while start <= num:
+                while count[start]:
+                    for i in range(start, start + groupSize):
+                        if not count[i]:
+                            return False
+                        count[i] -= 1
+                start += 1
+        return True
