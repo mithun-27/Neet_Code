d66be8ee9875d7417b04e6175487cc65ba3d1cc7
MITHUN <kvl202014@gmail.com>
2026-09-24 21:13:39 +0530

    neet code with answers

--- blind150/greedy/1899. Merge Triplets to Form Target Triplet.py
+++ blind150/greedy/1899. Merge Triplets to Form Target Triplet.py
@@ -36,3 +36,16 @@ Constraints:
 1 <= triplets.length <= 105
 triplets[i].length == target.length == 3
 1 <= ai, bi, ci, x, y, z <= 1000"""
+
+
+#answer:
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        x = y = z = False
+        for t in triplets:
+            x |= (t[0] == target[0] and t[1] <= target[1] and t[2] <= target[2])
+            y |= (t[0] <= target[0] and t[1] == target[1] and t[2] <= target[2])
+            z |= (t[0] <= target[0] and t[1] <= target[1] and t[2] == target[2])
+            if x and y and z:
+                return True
+        return False
