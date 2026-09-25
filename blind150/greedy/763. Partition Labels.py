4e08cf8b08d7d72068583daac79b1e1a80ce6943
MITHUN <kvl202014@gmail.com>
2026-09-25 20:56:16 +0530

    neet code with answers

--- blind150/greedy/763. Partition Labels.py
+++ blind150/greedy/763. Partition Labels.py
@@ -25,3 +25,22 @@ Constraints:
 
 1 <= s.length <= 500
 s consists of lowercase English letters."""
+
+
+#answer:
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        lastIndex = {}
+        for i, c in enumerate(s):
+            lastIndex[c] = i
+
+        res = []
+        size = end = 0
+        for i, c in enumerate(s):
+            size += 1
+            end = max(end, lastIndex[c])
+
+            if i == end:
+                res.append(size)
+                size = 0
+        return res
