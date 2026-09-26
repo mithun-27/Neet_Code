b7d61b322754ca49641e94cb634c479718f8cdf6
MITHUN <kvl202014@gmail.com>
2026-09-26 21:49:20 +0530

    neet code with answers

--- blind150/greedy/678. Valid Parenthesis String.py
+++ blind150/greedy/678. Valid Parenthesis String.py
@@ -31,3 +31,22 @@ Constraints:
 
 1 <= s.length <= 100
 s[i] is '(', ')' or '*'."""
+
+
+#answer:
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        leftMin, leftMax = 0, 0
+
+        for c in s:
+            if c == "(":
+                leftMin, leftMax = leftMin + 1, leftMax + 1
+            elif c == ")":
+                leftMin, leftMax = leftMin - 1, leftMax - 1
+            else:
+                leftMin, leftMax = leftMin - 1, leftMax + 1
+            if leftMax < 0:
+                return False
+            if leftMin < 0:
+                leftMin = 0
+        return leftMin == 0
